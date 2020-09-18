acfe7512d97f666e9b812734ce5fe3aeab3fa65a
Frazer Doyle <frazzles740@gmail.com>
2020-09-18 18:31:16 +0100

    Added initial build environment

---
+++ Quadopoly/Quadopoly/Quadopoly.cpp
@@ -0,0 +1,12 @@
+﻿// Quadopoly.cpp : Defines the entry point for the application.
+//
+
+#include "Quadopoly.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
